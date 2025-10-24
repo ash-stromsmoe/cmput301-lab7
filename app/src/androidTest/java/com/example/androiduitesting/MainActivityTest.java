94c8ecf909180a79fbbdc7daa993ff69776fcc6b
ash-stromsmoe <ashton.stromsmoe@gmail.com>
2025-10-23 18:18:53 -0600

    End of lab

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -1,7 +1,81 @@
 package com.example.androiduitesting;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
-    
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new
+            ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void testAddCity() {
+        // Click on Add City button
+        onView(withId(R.id.button_add)).perform(click());
+        // Type "Edmonton" in the editText
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        // Click on Confirm
+        onView(withId(R.id.button_confirm)).perform(click());
+        // Check if text "Edmonton" is matched with any of the text displayed on the screen
+        onView(withText("Edmonton")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testClearCity() {
+        // add city 1
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        onView(withId(R.id.button_confirm)).perform(click());
+        // add city 2
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Calgary"));
+        onView(withId(R.id.button_confirm)).perform(click());
+        // clear city
+        onView(withId(R.id.button_clear)).perform(click());
+        // check cities are gone
+        onView(withText("Edmonton")).check(doesNotExist());
+        onView(withText("Edmonton")).check(doesNotExist());
+
+        /* Code for stalling to see if initial steps of testing do what they should
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         */
+    }
+
+    @Test
+    public void TestListView() {
+        // add a city
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        onView(withId(R.id.button_confirm)).perform(click());
+        // check if it matches first element in list view
+        // we use onData for a different kind of element?
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list
+        )).atPosition(0).check(matches((withText("Edmonton"))));
+    }
 }
